831d8764b8692c20db4a3132bbfb2ee7a4dc30d7
Rakesh <rakesh.das@digitalwoods.net>
2024-10-06 22:22:56 +0530

    ggg

--- src/components/button.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Button.js
-import React from "react";
-
-const Button = ({ text, onClick }: any) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-export default Button;
