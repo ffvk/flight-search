d2fe207403d5b90491e4e28cb4cbab786b804036
Rakesh <rakesh.das@digitalwoods.net>
2024-10-06 22:18:30 +0530

    ggg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const IndexPage = () => {
     <>
       <main className="flex-grow container mx-auto p-6">
         <h1 className="text-3xl font-bold mb-4 flex justify-center">
-          Welcome to Flight Search
+          Welcome to Flight Search world
         </h1>
         <div className="border p-4 rounded-lg shadow-md">
           <h2 className="text-lg font-semibold mb-2 bg-gray-200 w-[6rem] flex justify-center">
